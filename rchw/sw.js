cfc6747bcd375f5b5818bef8e63824095064c3f8
coiner <91238055+colassn@users.noreply.github.com>
2026-09-03 14:36:04 +0800

    Add files via upload

--- rchw/sw.js
+++ rchw/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ehandbook-studyos-v2-1-1-verified';
+const CACHE_NAME = 'ehandbook-studyos-v2-2-0-motion';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
